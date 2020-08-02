9254ba7edfc29205c3bceb7edd0814749d70d370
puji ayu lestari <pujiayul24@gmail.com>
2020-08-03 00:21:37 +0700

    pert 11 : Membuat Halaman Template

---
+++ resources/views/mahasiswa/index.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.app')
+
+@section("title",'Halaman mahasiswa')
+@section("bread1",'mahasiswa')
+@section("bread2",'daftar')
+
+@section('content')
+    <p>This is my body content.</p>
+@endsection
